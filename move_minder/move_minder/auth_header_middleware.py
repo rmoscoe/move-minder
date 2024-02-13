2440e2353e112bf6b80fb96fc6a7d6d0067584f9
Ryan Moscoe <ryanmoscoe@gmail.com>
2024-02-13 09:59:11 -0800

    prepare settings and .env

---
+++ move_minder/move_minder/auth_header_middleware.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponseForbidden
+import os
+
+class AuthenticationHeaderMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+    
+    def __call__(self, request):
+        if request.method != 'GET':
+            header_value = request.headers.get("Authentication", None)
+            if header_value is None or header_value != os.environ.get('AUTHENTICATION'):
+                return HttpResponseForbidden("Access denied")
+        
+        response = self.get_response(request)
+        return response
--- move_minder/move_minder/settings.py
+++ move_minder/move_minder/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dal2ldmha^go44b@)@$^67lrp7%356b+#i%&x=vuxj*k!+zp0_'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('ENV') == 'DEV'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tracker'
 ]
 
 MIDDLEWARE = [
@@ -47,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'move_minder.auth_header_middleware.AuthenticationHeaderMiddleware'
 ]
 
 ROOT_URLCONF = 'move_minder.urls'
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'move_minder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT')
     }
 }
 
