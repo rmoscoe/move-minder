c5757aeb3bc20900287cc1d402839bc5360d7ed1
Ryan Moscoe <ryanmoscoe@gmail.com>
2024-02-14 13:30:49 -0800

    update URLs

--- move_minder/tracker/urls.py
+++ move_minder/tracker/urls.py
@@ -1,17 +1,23 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
-from .views import HomePageView, SignUpView, LoginView, DashboardView, UserListView, UserDetailView, MoveListView, MoveDetailView, ParcelListView, ParcelDetailView
+from .views import HomePageView, SignupView, LoginView, DashboardView, UserListView, UserDetailView, MoveListView, MoveDetailView, MoveUpdateView, MoveDeleteView, ParcelListView, ParcelDetailView, ParcelUpdateView, ParcelDeleteView
 
 app_name = "tracker"
 
 urlpatterns = [
     path("", HomePageView.as_View(), name="home"),
-    path("sign-up/", SignUpView.as_View(), name="sign-up"),
+    path("sign-up/", SignupView.as_View(), name="sign-up"),
     path("login/", LoginView.as_View(), name="login"),
+    path("logout/", LogoutView.as_view(), name="logout"),
     path("dashboard/", DashboardView.as_View(), name="dashboard"),
     path("users/", UserListView.as_View(), name="users-list"),
     path("users/<int:pk>/", UserDetailView.as_View(), name="user-detail"),
     path("moves/", MoveListView.as_View(), name="moves-list"),
     path("moves/<int:pk>/", MoveDetailView.as_view(), name="move-detail"),
+    path("moves/<int:pk>/edit/", MoveUpdateView.as_view(), name="move-update"),
+    path("moves/<int:pk>/delete/", MoveDeleteView.as_view(), name="move-delete"),
     path("parcels/", ParcelListView.as_view(), name="parcels-list"),
-    path("parcels/<int:pk>/", ParcelDetailView.as_view(), name="parcel-detail")
+    path("parcels/<int:pk>/", ParcelDetailView.as_view(), name="parcel-detail"),
+    path("parcels/<int:pk>/edit/", ParcelUpdateView.as_view(), name="parcel-update"),
+    path("parcels/<int:pk>/delete/", ParcelDeleteView.as_view(), name="parcel-delete")
 ]
--- move_minder/tracker/views.py
+++ move_minder/tracker/views.py
@@ -1,5 +1,47 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views import View
+from django.views.decorators.csrf import csrf_protect
+from django.views.generic import ListView, DetailView, FormView
 from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 # Create your views here.
+class HomePageView(View):
+    pass
+
+class SignupView(FormView):
+    pass
+
+class DashboardView(View, LoginRequiredMixin):
+    pass
+
+class UserListView(ListView, LoginRequiredMixin):
+    pass
+
+class UserDetailView(DetailView, LoginRequiredMixin):
+    pass
+
+class MoveListView(ListView, LoginRequiredMixin):
+    pass
+
+class MoveDetailView(DetailView, LoginRequiredMixin):
+    pass
+
+class MoveUpdateView(UpdateView, LoginRequiredMixin):
+    pass
+
+class MoveDeleteView(DeleteView, LoginRequiredMixin):
+    pass
+
+class ParcelListView(ListView, LoginRequiredMixin):
+    pass 
+
+class ParcelDetailView(DetailView, LoginRequiredMixin):
+    pass
+
+class ParcelUpdateView(UpdateView, LoginRequiredMixin):
+    pass
+
+class ParcelDeleteView(DeleteView, LoginRequiredMixin):
+    pass
