53504ea41c8d5f076d4153e5ceb966ac60ffc316
Ryan Moscoe <ryanmoscoe@gmail.com>
2024-04-23 14:56:21 -0700

    remove static directories from gitignore

---
+++ move_minder/static/tracker/js/form.js
@@ -0,0 +1,75 @@
+const setDivWidth = () => {
+    const form = document.querySelector("form");
+    const formDivs = form.querySelectorAll("div");
+
+
+    let setWidth = 0;
+    formDivs.forEach(div => {
+        div.classList.remove("block");
+        div.classList.add("inline-block");
+        div.removeAttribute("style");
+        const maxWidth = Math.max(setWidth, div.clientWidth);
+        const offset = div.offsetWidth;
+        const margin = offset - div.clientWidth;
+        const divParent = div.parentElement;
+        setWidth = Math.min(divParent.clientWidth - margin, maxWidth)
+    });
+
+    formDivs.forEach(div => {
+        div.style.width = setWidth + "px !important;";
+        div.classList.remove("inline-block");
+        div.classList.add("block");
+    });
+}
+
+window.onload = () => {
+    setDivWidth();
+    const formDivs = Array.from(document.querySelectorAll("form > div")).concat(Array.from(document.querySelectorAll("form > fieldset > div")));
+    formDivs.forEach(div => {
+        const label = div.querySelector("label");
+        const input = div.querySelector("input");
+        if (input?.hasAttribute("required")) {
+            label.classList.add("font-bold");
+            const labelText = label.innerText;
+            let newLabel = "";
+            for (let i = 0; i < labelText.length - 1; i++) {
+                newLabel += labelText[i];
+            }
+            newLabel += "*:";
+            label.innerText = newLabel;
+        }
+
+        const errorList = div.querySelector("ul.errorlist");
+        if (input && errorList) {
+            input?.classList.add("invalid");
+        } else {
+            input?.classList.remove("invalid");
+        }
+
+        const helpText = div.querySelector(".helptext");
+        if (input?.matches(":focus")) {
+            helpText?.classList.remove("hidden");
+            helpText?.classList.add("flex");
+        } else {
+            helpText?.classList.add("hidden");
+            helpText?.classList.remove("flex");
+        }
+
+        input?.addEventListener("focusout", (event) => {
+            if (!event.relatedTarget || !event.relatedTarget.closest('a[href]')) {
+                input?.classList.remove("invalid");
+                errorList?.remove();
+                helpText?.classList.add("hidden");
+                helpText?.classList.remove("flex");
+            }
+        });
+
+        input?.addEventListener("focusin", (event) => {
+            event.stopPropagation();
+            helpText?.classList.remove("hidden");
+            helpText?.classList.add("flex");
+        });
+    });
+}
+
+window.addEventListener("resize", () => setDivWidth());
---
+++ move_minder/static/tracker/js/header.js
@@ -0,0 +1,51 @@
+const hamburger = document.getElementById("nav-burger");
+const themeToggler = document.getElementById("theme-toggler");
+const accountMenuButton = document.getElementById("account-menu");
+const accountMenu = document.getElementById("account-menu-content");
+const overlay = document.getElementById("sidebar-overlay");
+const hamburgerMenu = document.getElementById("hamburger-menu");
+const closeBurger = document.getElementById("close-burger");
+
+hamburger?.addEventListener("click", () => {
+    overlay.classList.remove("hidden");
+    hamburgerMenu.classList.add("active");
+});
+
+themeToggler.addEventListener("click", () => {
+    if (localStorage.getItem('theme') === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        localStorage.theme = "light";
+        document.documentElement.classList.remove("dark");
+    } else {
+        localStorage.theme = "dark";
+        document.documentElement.classList.add("dark");
+    }
+});
+
+accountMenuButton?.addEventListener("click", () => {
+    accountMenu.classList.toggle("hidden");
+});
+
+overlay?.addEventListener("click", () => {
+    hamburgerMenu.classList.remove("active");
+    overlay.classList.add("hidden");
+});
+
+closeBurger?.addEventListener("click", () => {
+    overlay.classList.add("hidden");
+    hamburgerMenu.classList.remove("active");
+})
+
+document.addEventListener("click", (event) => {
+    let target = event.target;
+    if (!target.closest("#account-menu") && !target.closest("#account-menu")) {
+        accountMenu.classList.add("hidden");
+    }
+});
+
+document.addEventListener("click", (event) => {
+    let target = event.target;
+    if (!target.closest("#hamburger-menu") && !target.closest("#nav-burger")) {
+        overlay.classList.add("hidden");
+        hamburgerMenu.classList.remove("active");
+    }
+});
